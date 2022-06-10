de1d4263392fa5b513f48f633181fc1f80c01f4e
Doanftpaptech <doananhphamuk@gmail.com>
2022-06-10 10:42:33 +0700

    update

--- version1/Js.js
+++ version1/Js.js
@@ -2,8 +2,10 @@ const dropID = document.getElementById('dropID-menu');
 dropID.addEventListener('click', function() {
     const showMenu = document.getElementById('myDropdown')
     showMenu.classList.toggle('show')
-
-    // thay đổi vùng chọn
+        //an hien symbol
+    const symbolshow = document.querySelector('.symbol');
+    symbolshow.classList.toggle('showSylb')
+        // thay đổi vùng chọn
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
@@ -12,15 +14,21 @@ dropID.addEventListener('click', function() {
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
+        const symbolshow = document.querySelector('.symbol');
+
         var i;
         for (i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
+                symbolshow.classList.toggle('showSylb');
             }
         }
     }
 }
+
+
+
 
 // Add active class to the current button (highlight it)
 var header = document.getElementById("myDIV");
